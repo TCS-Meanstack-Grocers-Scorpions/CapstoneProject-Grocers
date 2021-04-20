95d11f5ae8ba1f6df09cbf3edcdeaf695c587cc0
AmeliaGraceBreithaupt <graceameliab@gmail.com>
2021-04-20 14:46:15 -0700

    Initial commit of grocers angular project. Skeleton of Project Created

---
+++ Grocers/Backend/Users/model/user.model.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+let UserSchema = mongoose.Schema({
+    fname:String,
+    lname:String,
+    email:String,
+    pass:String,
+    dob:Date,
+    phone:Number,
+    address:String,
+    funds:Number,
+    locked:Boolean
+})
+
+let UserModel = mongoose.model("",UserSchema,"User")
+
+module.exports = UserModel;
