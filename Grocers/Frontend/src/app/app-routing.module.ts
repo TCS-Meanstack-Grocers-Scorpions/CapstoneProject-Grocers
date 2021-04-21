ffc51f7810743f6e912ef1cc0af9704cdf772d7d
AmeliaGraceBreithaupt <graceameliab@gmail.com>
2021-04-21 14:26:39 -0700

    added buttons to initial page

--- Grocers/Frontend/src/app/app-routing.module.ts
+++ Grocers/Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent as AdminSignin } from './Admin/sign-in/sign-in.component';
+import { SignInComponent as EmployeeSignin } from './Employee/sign-in/sign-in.component';
+import { SignInComponent as UserSignin } from './User/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"\admin",component:AdminSignin},
+  {path:"\shopper",component:UserSignin},
+  {path:"\employee",component:EmployeeSignin},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Grocers/Frontend/src/app/app.component.ts
+++ Grocers/Frontend/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Grocers';
+  isShown:boolean = true;
+
+  hide(){
+    this.isShown = false;
+    
+  }
 }
