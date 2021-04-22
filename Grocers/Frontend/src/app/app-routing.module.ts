4bf202f0bfaa6612f022e7889db7e2b9bfa647ae
AmeliaGraceBreithaupt <graceameliab@gmail.com>
2021-04-22 11:58:24 -0700

    admin signin button to go to admin-index

--- Grocers/Frontend/src/app/app-routing.module.ts
+++ Grocers/Frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminIndexComponent } from './Admin/admin-index/admin-index.component';
 import { SignInComponent as AdminSignin } from './Admin/sign-in/sign-in.component';
 import { SignInComponent as EmployeeSignin } from './Employee/sign-in/sign-in.component';
 import { SignInComponent as UserSignin } from './User/sign-in/sign-in.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   {path:"\admin",component:AdminSignin},
   {path:"\shopper",component:UserSignin},
   {path:"\employee",component:EmployeeSignin},
+  {path:"\admin-index",component:AdminIndexComponent}
 ];
 
 @NgModule({
