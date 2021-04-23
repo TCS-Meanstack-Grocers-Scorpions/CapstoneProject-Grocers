4ee29fc9b8b6f255c38c14803c562d9e0a88d3ce
Liam Andrews-Bancroft <liam.andrews-bancroft@du.edu>
2021-04-23 16:14:57 -0600

    updated employee index page

--- Grocers/Frontend/src/app/app-routing.module.ts
+++ Grocers/Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeIndexComponent } from './Employee/employee-index/employee-index.component';
+import { SendRequestComponent } from './Employee/employee-index/send-request/send-request.component';
+import { UnlockUsersComponent } from './Employee/employee-index/unlock-users/unlock-users.component';
+import { UpdateOrderStatusComponent } from './Employee/employee-index/update-order-status/update-order-status.component';
+import { EditProdileComponent } from './User/user-index/edit-profile/edit-prodile.component';
 
 const routes: Routes = [
-  {path:"\employeeDashboard",component:EmployeeIndexComponent}
+  {path:"\employeeDashboard",component:EmployeeIndexComponent},
+  {path:"\editEmployeeProfile",component:EditProdileComponent},
+  {path:"\sendRequest",component:SendRequestComponent},
+  {path:"\nlockUsers",component:UnlockUsersComponent},
+  {path:"\pdateOrderStatus",component:UpdateOrderStatusComponent}
 ];
 
 @NgModule({
