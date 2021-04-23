0f68cec233b7756b048fefd4ca422d32455eb668
AmeliaGraceBreithaupt <graceameliab@gmail.com>
2021-04-23 12:25:36 -0700

    added product table and button to admin-index

--- Grocers/Frontend/src/app/Admin/admin-index/admin-index.component.ts
+++ Grocers/Frontend/src/app/Admin/admin-index/admin-index.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from 'src/app/model.product';
+import { ProductService } from 'src/app/product.service';
 
 @Component({
   selector: 'app-admin-index',
@@ -7,13 +9,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin-index.component.css']
 })
 export class AdminIndexComponent implements OnInit {
-
-  constructor(public router:Router) { }
+  showTable:Boolean = false;
+  products?:Array<Product>
+  constructor(public router:Router, public proService:ProductService) { }
 
   ngOnInit(): void {
   }
 
   open(rout:any) {
     this.router.navigate(["admin-employee"]);
+  }
+
+  toggleTable(){
+    this.showTable = !this.showTable;
+    if(this.showTable){
+      this.proService.retrieveAllProductDetails().subscribe(result=>this.products=result);
+    }
   }
 }
