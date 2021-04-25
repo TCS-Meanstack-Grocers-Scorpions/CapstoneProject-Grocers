686782f38504d6fde549e1421f4e250af7f37b24
Andrew <andrew1nwalie@gmail.com>
2021-04-24 19:00:38 -0500

    Select items Completed

--- Grocers/Backend/Users/router/User.router.js
+++ Grocers/Backend/Users/router/User.router.js
@@ -6,6 +6,7 @@ let UserController = require("../controller/users.controller.js");
 router.post("/signUp", UserController.storeUserDetails);
 router.post("/ticket",UserController.raiseTicket);
 router.get("/select", UserController.selectObject);
+router.post("/select",UserController.addtoCart);
 
 
 module.exports = router;
