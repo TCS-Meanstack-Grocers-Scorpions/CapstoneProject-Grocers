1db9c542543614ca53243de83f575164708600aa
Andrew <andrew1nwalie@gmail.com>
2021-04-27 17:37:20 -0500

    fixed what i broke

--- Grocers/Backend/Users/router/User.router.js
+++ Grocers/Backend/Users/router/User.router.js
@@ -4,8 +4,6 @@ let router = express.Router();
 let UserController = require('../controller/users.controller');
 
 router.post('/signUp', UserController.storeUserDetails);
-
-
 router.get("/getUserById/:pid", UserController.getUserById);
 router.put("/updateUserPassword/:uid",UserController.updateUserPassword);
 router.put("/updateUserEmail/:uid",UserController.updateUserEmail);
@@ -21,11 +19,7 @@ router.get("/cart/:userId",UserController.viewCart);
 router.put("/cart",UserController.updateCart);
 router.delete("/cart/:pid",UserController.deleteCart);
 //router.get("/orderStatus:/:uid",UserController.getOrderStatus);
-
-router.post('/signUp', UserController.storeUserDetails);
-router.get('/getUserById/:pid', UserController.getUserById);
 router.put('/lockUser', UserController.lockUser);
 router.put('/unlockUsers/', UserController.unlockUser);
-router.post('/ticket', UserController.raiseTicket);
-router.get('/select', UserController.selectObject);
-router.post('/select', UserController.addtoCart);
+
+module.exports = router;
