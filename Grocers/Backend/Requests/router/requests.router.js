2ea3316fa6d740ff9dcf9fa0e5b07fa13a5c6031
AmeliaGraceBreithaupt <graceameliab@gmail.com>
2021-04-27 08:11:37 -0700

    added request delete function

--- Grocers/Backend/Requests/router/requests.router.js
+++ Grocers/Backend/Requests/router/requests.router.js
@@ -4,5 +4,6 @@ let RequestController = require('../controller/requests.controller.js');
 
 router.get('/getRequests', RequestController.getRequests);
 router.post('/sendRequest', RequestController.saveRequest);
+router.post('/deleteRequest', RequestController.deleteRequestById);
 
 module.exports = router;
