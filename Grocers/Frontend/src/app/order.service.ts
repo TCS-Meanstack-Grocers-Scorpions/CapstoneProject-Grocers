97d617b3e3715ba40e5fa59f57b414ca388389c7
lechestercat <chesterortega1991@gmail.com>
2021-04-27 04:30:41 -0400

    added update order status component but does not update records yet

--- Grocers/Frontend/src/app/order.service.ts
+++ Grocers/Frontend/src/app/order.service.ts
@@ -3,10 +3,23 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService {
+  constructor(public http: HttpClient) {}
 
-  constructor() { }
-  getOrderByUserID(){}
+  getOrderByUserID() {}
+
+  // update order status
+  updateOrderStatus(updateRef: any): any {
+    console.log('updateRef: ' + updateRef);
+    this.http
+      .put('http://localhost:9090/order/updateOrderStatus', updateRef, {
+        responseType: 'text',
+      })
+      .subscribe(
+        (result) => console.log(result),
+        (error) => console.log(error)
+      );
+  }
 }
