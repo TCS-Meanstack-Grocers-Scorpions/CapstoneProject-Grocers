c6e0597e9b46171b4e54cd431714af8c07e58e65
Andrew <andrew1nwalie@gmail.com>
2021-04-27 23:50:37 -0500

    item model

--- Grocers/Frontend/src/app/model.purchase.ts
+++ Grocers/Frontend/src/app/model.purchase.ts
@@ -1,7 +1,9 @@
+import { Items } from "./model.items";
+
 export class Purchased {
                                                     //format datePurchased:"4/27/2021"
-    constructor(public userId:string,public items=[{}]){ }
-     //items[{pid:productId,name:productname,quantity:quantityPurchased,price:productPrice,datePurchased:"4/27/2021"},orderStatus:"processing", orderTotal:20]; 
+    constructor(public userId:string,public items:Array<Items>){ }
+     //items[{pid:productId,name:productname,quantity:quantityPurchased,price:productPrice,datePurchased:"4/27/2021",orderStatus:"processing", orderTotal:20}]; 
 
 
 }
