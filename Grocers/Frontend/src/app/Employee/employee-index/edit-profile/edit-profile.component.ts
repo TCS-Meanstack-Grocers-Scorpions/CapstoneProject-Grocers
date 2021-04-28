e141ae37e0c15b70df982f24bf9dd8e1dbdc33e4
AmeliaGraceBreithaupt <graceameliab@gmail.com>
2021-04-28 10:58:41 -0700

    Revert "Merge branch 'master' into Liam"
    This reverts commit f4d8171dbb94d5366f1f9b3e4b39391b76427a94, reversing
    changes made to 5894056b62084a8ff77074d244a2ae4e683f5e76.

--- Grocers/Frontend/src/app/Employee/employee-index/edit-profile/edit-profile.component.ts
+++ Grocers/Frontend/src/app/Employee/employee-index/edit-profile/edit-profile.component.ts
@@ -1,19 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { EmployeeService } from '../../../employee.service';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
-  styleUrls: ['./edit-profile.component.css'],
+  styleUrls: ['./edit-profile.component.css']
 })
 export class EditProfileComponent implements OnInit {
-  constructor(public empServ: EmployeeService) {}
-  msg?: String;
 
-  ngOnInit(): void {}
+  constructor() { }
 
-  updateEmpProfile(editRef: any): void {
-    this.empServ.updateEmployeeProfile(editRef);
-    this.msg = 'Your profile is updated!';
+  ngOnInit(): void {
   }
+
 }
