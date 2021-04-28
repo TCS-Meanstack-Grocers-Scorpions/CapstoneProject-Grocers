0692ee2c7d26b10201423f3f7ed8ce348906de5c
lechestercat <chesterortega1991@gmail.com>
2021-04-28 12:50:06 -0400

    Added edit Employee profile component

--- Grocers/Frontend/src/app/Employee/employee-index/edit-profile/edit-profile.component.ts
+++ Grocers/Frontend/src/app/Employee/employee-index/edit-profile/edit-profile.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../../employee.service';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
-  styleUrls: ['./edit-profile.component.css']
+  styleUrls: ['./edit-profile.component.css'],
 })
 export class EditProfileComponent implements OnInit {
+  constructor(public empServ: EmployeeService) {}
+  msg?: String;
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  updateEmpProfile(editRef: any): void {
+    this.empServ.updateEmployeeProfile(editRef);
+    this.msg = 'Your profile is updated!';
   }
-
 }
