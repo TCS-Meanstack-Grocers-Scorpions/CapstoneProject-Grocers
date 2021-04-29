cf40525275a1e02083bb657023d2c9f887c528cd
AmeliaGraceBreithaupt <graceameliab@gmail.com>
2021-04-29 15:56:44 -0700

    update user has success messages

--- Grocers/Frontend/src/app/User/user-index/edit-profile/edit-prodile.component.ts
+++ Grocers/Frontend/src/app/User/user-index/edit-profile/edit-prodile.component.ts
@@ -9,6 +9,7 @@ import { UsersService } from 'src/app/users.service';
   styleUrls: ['./edit-prodile.component.css']
 })
 export class EditProdileComponent implements OnInit {
+  success=''
   id = sessionStorage.getItem('curUserId');
   constructor(public userSer: UsersService) { }
 
@@ -17,18 +18,33 @@ export class EditProdileComponent implements OnInit {
 
   updatePass(userRef: any): void{
     this.userSer.updateUserPassword(userRef, this.id);
+    this.success = 'Password Updated Successfully'
+    setTimeout(()=>
+    this.success='',8000)
   }
   updateEmail(userRef: any): void{
     this.userSer.updateUserEmail(userRef, this.id);
+    this.success = 'Email Updated Successfully'
+    setTimeout(()=>
+    this.success='',8000)
   }
   updateAddress(userRef: any): void{
     this.userSer.updateUserAddress(userRef, this.id);
+    this.success = 'Address Updated Successfully'
+    setTimeout(()=>
+    this.success='',8000)
   }
   updatePhone(userRef: any): void{
     this.userSer.updateUserPhone(userRef, this.id);
+    this.success = 'Phone Updated Successfully'
+    setTimeout(()=>
+    this.success='',8000)
   }
   updateDOB(userRef: any): void{
     this.userSer.updateUserDOB(userRef, this.id);
+    this.success = 'DOB Updated Successfully'
+    setTimeout(()=>
+    this.success='',8000)
   }
 
 }
