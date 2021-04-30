51a225ffe447ffe91bada6212b014bb952473d0d
Andrew <andrew1nwalie@gmail.com>
2021-04-30 11:24:55 -0500

    order table

--- Grocers/Frontend/src/app/Employee/employee-index/update-order-status/update-order-status.component.ts
+++ Grocers/Frontend/src/app/Employee/employee-index/update-order-status/update-order-status.component.ts
@@ -12,7 +12,7 @@ import { UsersService } from 'src/app/users.service';
 export class UpdateOrderStatusComponent implements OnInit {
   constructor(public updateOr: OrderService) {}
   msg?: string;
- orders?:Array<Order>
+ orders:Array<Order>=[]
   ngOnInit(): void {
     this.updateOr.getAllOrders().subscribe(result=>{console.log(result); this.orders=result}); // gets all orders and console logs it on load, probably move this into a table
   }
@@ -20,5 +20,6 @@ export class UpdateOrderStatusComponent implements OnInit {
   updateOrder(updateRef: any): void {
     this.updateOr.updateOrderStatus(updateRef);
     this.msg = 'Order Updated!';
+    setTimeout(function() { location.reload(); }, 500);
   }
 }
